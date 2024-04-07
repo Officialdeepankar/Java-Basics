6465fdd7497a14ce487cf55b36730adb61823cee
Deepankar singh <singhdeepankarpost@gmail.com>
2024-04-08 02:13:41 +0530

    "packages"

---
+++ _05 packtest/packtest.java
@@ -0,0 +1,14 @@
+package firstpackage;
+
+
+/* 
+ * To classify one type of class in one directory and another type of classes in other directory we use concepts of packages;
+ * commands- 1) javac   filename.java
+ * command -2) javac -d . filename.java (this will create a folder that will contain all the classes related ot packtest file )
+ */
+public class packtest {
+    public static void main (String args[])
+    {
+        System.out.println("Hello!This is deepankar singh!");
+    }
+}
